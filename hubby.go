6d0476dc90ee86edd347f1fe9a47a0bf41ed319e
Stian Ravndal <stian.ravndal@outlook.com>
2020-09-14 21:30:44 +0200

    fix struct

--- hubby.go
+++ hubby.go
@@ -192,11 +192,11 @@ type Domain struct {
 //DNSRecord ...
 type DNSRecord struct {
 	ID       int    `json:"id,omitempty"`
-	Host     string `json:"host"`
-	TTL      int    `json:"ttl"`
-	Type     string `json:"type"`
-	Data     string `json:"data"`
-	Priority int16  `json:"priority"`
-	Weight   int16  `json:"weight"`
-	Port     int16  `json:"port"`
+	Host     string `json:"host,omitempty"`
+	TTL      int    `json:"ttl,omitempty"`
+	Type     string `json:"type,omitempty"`
+	Data     string `json:"data,omitempty"`
+	Priority int16  `json:"priority,omitempty"`
+	Weight   int16  `json:"weight,omitempty"`
+	Port     int16  `json:"port,omitempty"`
 }
