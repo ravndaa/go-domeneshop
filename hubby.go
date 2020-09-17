2bfe3cdbbecdc94f6d92b3d6db6523d6ba2abfae
Stian Ravndal <stian.ravndal@outlook.com>
2020-09-17 13:08:34 +0200

    more data

--- hubby.go
+++ hubby.go
@@ -27,7 +27,7 @@ func basicAuth(username, password string) string {
 
 //New domeneshop client.
 func New(clientid string, clientsecret string, client *http.Client) *Hubby {
-	fmt.Println(basicAuth(clientid, clientsecret))
+
 	api := Hubby{
 		auth:   basicAuth(clientid, clientsecret),
 		client: client,
@@ -67,27 +67,47 @@ func (a *Hubby) GetDomains() ([]Domain, error) {
 
 }
 
-//GetDomainDNSRecords ...
-func (a *Hubby) GetDomainDNSRecords(domainid int) ([]DNSRecord, error) {
+//FindDomains with filter
+func (a *Hubby) FindDomains(filter string) ([]Domain, error) { return nil, nil }
+
+//FindDomain using id
+func (a *Hubby) FindDomain(domainid string) ([]Domain, error) { return nil, nil }
+
+//ListDNSRecords ...
+func (a *Hubby) ListDNSRecords(domainid int, host string, dnstype string) ([]DNSRecord, error) {
 	url := fmt.Sprintf("%v/domains/%v/dns", baseURL, domainid)
+	// add some queries if not empty
+	if dnstype != "" && host != "" {
+		url = fmt.Sprintf("%v?host=%v&type=%v", url, host, dnstype)
+	} else if host != "" {
+		url = fmt.Sprintf("%v?host=%v", url, host)
+	} else if dnstype != "" {
+		url = fmt.Sprintf("%v?typ=%v", url, dnstype)
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
+	// Set some request header data, one required and one for the fun.
 	req.Header.Set("User-Agent", "ravndaa/hubby")
 	req.Header.Add("Authorization", "Basic "+a.auth)
+
+	// ask the api nicely to get some dns records.
 	resp, err := a.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	// check if return body is nil, or move on.
 	if resp.Body != nil {
 		defer resp.Body.Close()
 	}
+	// read the response body since it isnt nil.
 	body, readErr := ioutil.ReadAll(resp.Body)
 	if readErr != nil {
 		return nil, err
 	}
+	// create an Dnsrecord array used by json unmarshal.
 	records := []DNSRecord{}
 	err = json.Unmarshal(body, &records)
 	if err != nil {
@@ -99,6 +119,33 @@ func (a *Hubby) GetDomainDNSRecords(domainid int) ([]DNSRecord, error) {
 //AddDNSRecord ...
 func (a *Hubby) AddDNSRecord(domainid int, value DNSRecord) error {
 	url := fmt.Sprintf("%v/domains/%v/dns", baseURL, domainid)
+
+	if value.Host == "" {
+		return errors.New("missing host")
+	}
+	if value.Type == "" {
+		return errors.New("mssing type")
+	}
+	if value.Data == "" {
+		return errors.New("missing data")
+	}
+
+	switch DNSType := value.Type; DNSType {
+	case "MX":
+		if value.Priority == "" {
+			return errors.New("missing priority")
+		}
+	case "SRV":
+		if value.Priority == "" {
+			return errors.New("missing priority")
+		}
+		if value.Weight == "" {
+			return errors.New("missing weight")
+		}
+		if value.Port == "" {
+			return errors.New("missing port")
+		}
+	}
 
 	payload := new(bytes.Buffer)
 	json.NewEncoder(payload).Encode(value)
@@ -114,12 +161,17 @@ func (a *Hubby) AddDNSRecord(domainid int, value DNSRecord) error {
 	if err != nil {
 		return err
 	}
-	if resp.StatusCode != 201 {
-		fmt.Println(resp)
-
-	}
+	// not sure why I have copied this in, should check it some time.
 	if resp.Body != nil {
 		defer resp.Body.Close()
+	}
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		return err
+	}
+
+	if resp.StatusCode >= 300 {
+		return fmt.Errorf("%s", body)
 	}
 
 	return nil
@@ -180,13 +232,23 @@ func (a *Hubby) DeleteDNSRecord(domainid int, dnsrecordid int) error {
 
 //Domain ..
 type Domain struct {
-	ID             int    `json:"id"`
-	Domain         string `json:"domain"`
-	Status         string `json:"status"`
-	ExpiryDate     string `json:"expiry_date"`
-	RegisteredDate string `json:"registered_date"`
-	Renew          bool   `json:"renew"`
-	Registrant     string `json:"registrant"`
+	ID             int      `json:"id"`
+	Domain         string   `json:"domain"`
+	Status         string   `json:"status"`
+	ExpiryDate     string   `json:"expiry_date"`
+	RegisteredDate string   `json:"registered_date"`
+	Renew          bool     `json:"renew"`
+	Registrant     string   `json:"registrant"`
+	Nameservers    []string `json:"nameservers"`
+	Services       Service  `json:"services"`
+}
+
+//Service ...
+type Service struct {
+	Registrar bool   `json:"registrar"`
+	Dns       bool   `json:"dns"`
+	Email     bool   `json:"email"`
+	Webhotel  string `json:"webhotel"`
 }
 
 //DNSRecord ...
--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@
 ![Go](https://github.com/ravndaa/hubby/workflows/Go/badge.svg)
 
 Docs: https://api.domeneshop.no/docs/
+
+
+### Todo:
+1. Create httpclient for adding required fields used by every request. ()
